c698c4c6a1a989b6042a7746ac1246c027a27877
itsvitaliio <itsvitaliio@gmail.com>
2021-09-18 17:49:46 -0400

    Set up the routers

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,12 @@ app.use(cors(options));
 app.use(express.json());
 
 app.use("/", require('./routes/authRouter.js'));
-app.use("/", require('./routes/worker.serviceRouters'));
-app.use("/", require('./routes/authRouter.js'));
+app.use("/", require('./routes/currentJobRouter.js'));
+app.use("/", require('./routes/matchingRouter.js'));
+app.use("/", require('./routes/offerRouter.js'));
+app.use("/", require('./routes/ratingRouter.js'));
 app.use("/", require('./routes/userRouter.js'));
+app.use("/", require('./routes/worker.serviceRouter.js'));
 
 app.listen(process.env.PORT, ()=>{
     console.log('Running on port '+ process.env.PORT);
